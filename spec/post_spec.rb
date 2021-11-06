e96b718e8e2f73319b44818598f3dc349de09955
片川良平 <kyatakawa1230@gmail.com>
2021-11-06 15:04:08 +0900

    feat: rspecの導入

---
+++ spec/post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context "タイトルと内容が入力されている場合" do
+    let!(:post) do
+      Post.new({ title: "Rspecのテスト", content: "try RSpec" })
+    end
+
+    it '記事を保存できる' do
+      expect(post).to be_valid
+    end
+  end
+end
